f1dcc29050fac29f7402ebbf79623c94e8d4a3fa
doraeric <benson.doraemon@gmail.com>
2018-04-27 21:45:15 +0800

    add hook movement controll

---
+++ Assets/Scripts/Hook.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(ConfigurableJoint))]
+[RequireComponent(typeof(Rigidbody))]
+
+public class Hook : MonoBehaviour {
+	ConfigurableJoint joint2trolley;
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		joint2trolley = GetComponent<ConfigurableJoint>();
+		rb = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("left shift")){
+			if(joint2trolley.anchor.y < 16f) {
+				joint2trolley.anchor += Vector3.up * Time.deltaTime;
+				rb.WakeUp();
+			}
+		}
+		if(Input.GetKey("space")){
+			if(joint2trolley.anchor.y > .7f) {
+				joint2trolley.anchor -= Vector3.up * Time.deltaTime;
+				rb.WakeUp();
+			}
+		}
+	}
+}
